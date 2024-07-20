a5b3495bd812a168fd7d96b0811ad161b34758ca
DayanaraChuraco <cdchuraco@uce.edu.ec>
2024-07-20 08:52:57 -0500

    yml

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/routes');
 const app = express();
-const port = 3000;
+const port = 5000;
 
 // Conectar a MongoDB
 mongoose.connect('mongodb://localhost:27017/users_db', {
