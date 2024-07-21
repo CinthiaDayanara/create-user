415a5b2e06b363467d6d0ac20c3da768c744f3e5
SaraSaragozin <saralizeth0711@gmail.com>
2024-07-21 18:17:40 -0500

    Add CORS for conexion

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const userRoutes = require('./routes/routes');
 const app = express();
 const port = 3000;
@@ -10,6 +11,11 @@ mongoose.connect('mongodb://localhost:27017/users_db', {
 })
 .then(() => console.log('Conectado a MongoDB'))
 .catch((error) => console.error('Error al conectar a MongoDB:', error));
+
+app.use(cors({
+  origin: '*', // Permite solicitudes desde esta URL
+  methods: 'GET,POST,PUT,DELETE', // Métodos HTTP permitidos
+}));
 
 app.use(express.json());
 
